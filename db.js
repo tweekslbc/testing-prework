07a0fa6b631e3f41b60a4e45576e1809941614be
EC2 Default User <ec2-user@ip-172-31-5-253.us-east-2.compute.internal>
2019-09-20 23:25:14 +0000

    video 4

--- db.js
+++ db.js
@@ -1,19 +1,40 @@
 const Sequelize = require('sequelize');
-const { STRING } = Sequelize;
+const { STRING, UUID, UUIDV4 } = Sequelize;
 
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_tdd_db');
 
 const Product = conn.define('product', {
+    id: {
+        primaryKey: true,
+        type: UUID,
+        defaultValue: UUIDV4
+    },
     name: STRING
 });
 
+const Category = conn.define('category', {
+       id: {
+        primaryKey: true,
+        type: UUID,
+        defaultValue: UUIDV4
+    },
+    name: STRING
+});
+
+Product.belongsTo(Category);
 
 const syncAndSeed = async() => {
     await conn.sync({ force: true });
+  const categories = [
+    { name: 'catFoo'}, 
+    { name: 'catBar'},
+    { name: 'catBazz'}
+]; 
+ const [ catFoo, catBar, catBazz ] = await Promise.all(categories.map(category => Category.create(category)));
   const products = [
-    { name: 'foo'}, 
-    { name: 'bar'},
-    { name: 'bazz'}
+    { name: 'foo', categoryId: catFoo.id}, 
+    { name: 'bar', categoryId: catBar.id},
+    { name: 'bazz', categoryId: catBazz.id}
 ]; 
  const [foo, bar, bazz ] = await Promise.all(products.map(product => Product.create(product)));
  return {
@@ -21,6 +42,11 @@ const syncAndSeed = async() => {
          foo,
          bar,
          bazz
+     },
+     categories: {
+         catFoo,
+         catBar,
+         catBazz
      }
  }
 };
